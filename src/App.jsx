dd96a030d3780ab1763858d17c9de29a530fa050
SKRT7-0day <xdbronah@gmail.com>
2026-05-13 18:44:46 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect } from 'react';
 import { Tag, Moon, Sun, ExternalLink, LogIn } from 'lucide-react';
 
@@ -79,4 +80,3 @@ function App() {
 }
 
 export default App;
-
