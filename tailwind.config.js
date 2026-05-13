55219031df6b0fd19bbb86771e73e93b7049048e
SKRT7-0day <xdbronah@gmail.com>
2026-05-13 11:10:07 +0100

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'discord-purple': '#5865F2',
+      }
+    },
+  },
+  plugins: [],
+}
+
